e8ea381261f0863ed074de1cd250022fe4262b5d
Tikaram Mardi <tikarammardi007@gmail.com>
2019-09-21 18:17:10 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID:
-    '553877309018-ufvtn0dhnhg0afh0kfkrncusd6019dpf.apps.googleusercontent.com',
-  googleClientSecret: 'CYyEnumLYoktBNVdzjQmsIZ2',
-  mongoURI: 'mongodb://tikaram:password123@ds117935.mlab.com:17935/myemail-dev',
-  cookieKey: 'mysercetjlkjjl',
-  stripePublishableKey: 'pk_test_7XKGLvfkrePi2dYAgUcd5gNE00PTUU7z4z',
-  stripeSecreteKey: 'sk_test_LDaiEWIBWrC6liKKWH1msdcb00iJAmysHM'
-};
