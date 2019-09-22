d91e47dd779a2a25f12379b8cac0f60af60b51d7
Tikaram Mardi <tikarammardi007@gmail.com>
2019-09-22 19:14:34 +0530

    bugs bugs bugs bugs lets fix them

--- index.js
+++ index.js
@@ -30,6 +30,16 @@ require('./routes/authRoutes')(app);
 require('./routes/bookingRoutes')(app);
 require('./routes/jsonDataRoute')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express serving up production assets
+  app.use(express.static('client/build'));
+  //Express serving up the index.html file if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
